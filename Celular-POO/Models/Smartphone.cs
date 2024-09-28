d03e11eb861728d5bbc6610e6d363ff1e01ee8c8
Xounder <renanfellipe1@hotmail.com>
2024-09-27 22:15:28 -0300

    created Celular-POO program

---
+++ Celular-POO/Models/Smartphone.cs
@@ -0,0 +1,30 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        private string modelo;
+        private string imei;
+        private int memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
+        }
+        
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Celular-POO/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia =  new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1234", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone =  new Iphone(numero: "144856", modelo: "Modelo 2", imei: "1848", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
